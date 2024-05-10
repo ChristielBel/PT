9501cb59bf1efc0cf6429343118ca08814b76457
Maksim Shiryaev <shimady563@gmail.com>
2024-05-10 19:50:02 +0300

    feat(generation): add base generation files

---
+++ generation/odd/generate_odd_var.py
@@ -0,0 +1,11 @@
+import generation.generate_var as generate_var
+
+# List for all generating functions
+task_functions = [
+]
+ans_functions = [
+]
+
+
+def generate_odd(check_values, doc_path, ans_path, num):
+    generate_var.generate_var(task_functions, ans_functions, check_values, doc_path, ans_path, num)
---
+++ generation/writer.py
@@ -0,0 +1,45 @@
+import os
+
+import docx
+from docx import Document
+from docx.shared import Pt
+
+
+def write_text(doc_path, text, font_name, font_size, alignment, is_bold):
+    doc = docx.Document(doc_path)
+
+    paragraph = doc.add_paragraph()
+    paragraph.alignment = alignment
+
+    run = paragraph.add_run(text)
+    font = run.font
+    font.name = font_name
+    font.size = Pt(font_size)
+    font.bold = is_bold
+
+    doc.save(doc_path)
+
+
+def replace_placeholders_and_write_to_target(source_doc_path, target_doc_path, replacement_values):
+    # Open the source document
+    source_doc = Document(os.path.abspath(source_doc_path))
+
+    # Initialize index to track replacement value
+    replacement_index = 0
+
+    # Clone the source document
+    target_doc = Document(target_doc_path)
+
+    # Iterate over paragraphs
+    for paragraph in source_doc.paragraphs:
+        # Place all values to all paragraphs
+        while paragraph.text.find("+") != -1:
+            paragraph.text = paragraph.text.replace("+", str(replacement_values[replacement_index]), 1)
+
+            replacement_index += 1
+
+        target_doc.add_paragraph(paragraph.text)
+
+    # Save the modified document
+    target_doc.save(target_doc_path)
+
