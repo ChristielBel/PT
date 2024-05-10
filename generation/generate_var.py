5d67824c475ed6d5bed3f9ab23065122213578ae
Maksim Shiryaev <shimady563@gmail.com>
2024-05-10 14:06:41 +0300

    feat(generation): move generation login to the separate file

---
+++ generation/generate_var.py
@@ -0,0 +1,20 @@
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT as align
+
+from generation import writer
+
+
+def generate_var(task_functions, ans_functions, check_values, doc_path, ans_path, num):
+    # Writing header
+    if num == 1:
+        header = "Вариант " + str(num)
+    else:
+        header = "Вариант " + str(num) + '\n'
+
+    writer.write_text(doc_path, header, "Arial", 14, align.CENTER, True)
+    writer.write_text(ans_path, header, "Arial", 14, align.CENTER, True)
+
+    # Starting generating new var
+    for i in range(len(task_functions)):
+        if check_values[i].get():
+            task_functions[i](doc_path)
+            ans_functions[i](ans_path)
--- generation/odd/generate_odd_var.py
+++ generation/odd/generate_odd_var.py
@@ -1,4 +1,4 @@
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT as align
+import generation.generate_var as generate_var
 import generation.odd.tasks.task1.task_1 as task_1
 import generation.odd.tasks.task2.task_2 as task_2
 import generation.odd.tasks.task3.task_3 as task_3
@@ -17,8 +17,6 @@ import generation.odd.tasks.task15.task_15 as task_15
 import generation.odd.tasks.task16.task_16 as task_16
 import generation.odd.tasks.task17.task_17 as task_17
 
-import generation.writer as writer
-
 # List for all generating functions
 task_functions = [
     task_1.generate_task,
@@ -35,17 +33,4 @@ ans_functions = [
 
 
 def generate_odd(check_values, doc_path, ans_path, num):
-    # Writing header
-    if num == 1:
-        header = "Вариант " + str(num)
-    else:
-        header = "Вариант " + str(num) + '\n'
-
-    writer.write_text(doc_path, header, "Arial", 14, align.CENTER, True)
-    writer.write_text(ans_path, header, "Arial", 14, align.CENTER, True)
-
-    # Starting generating new var
-    for i in range(len(task_functions)):
-        if check_values[i].get():
-            task_functions[i](doc_path)
-            ans_functions[i](ans_path)
+    generate_var.generate_var(task_functions, ans_functions, check_values, doc_path, ans_path, num)
