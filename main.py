b438ef2b9f9c92204fb030970cac7996f2ed01ec
ChristinaBel <kristina04bel@gmail.com>
2024-05-12 18:11:32 +0300

    brought beauty

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ class MyApp:
     def __init__(self, root, title_font, regular_font):
         self.root = root
         self.root.title("Генератор")
-        self.root.geometry("900x500")
+        self.root.geometry("900x530")
+        self.root.config(bg="#ADD8E6")
         self.title_font = title_font
         self.regular_font = regular_font
         self.check_vals = []
@@ -22,19 +23,24 @@ class MyApp:
 
     def create_widgets(self):
         # Frame to contain the entry field
-        entry_frame = tk.Frame(self.root)
+        entry_frame = tk.Frame(self.root, bg="#ADD8E6")
         entry_frame.pack(pady=20)
 
         # Label for the entry field
-        entry_label = tk.Label(entry_frame, text="Количество вариантов:", font=self.title_font)
+        entry_label = tk.Label(entry_frame, text="Количество вариантов:", font=self.title_font, bg="#ADD8E6")
         entry_label.grid(row=0, column=0)
 
         # Entry field
-        entry_field = tk.Entry(entry_frame, textvariable=self.entry_var)
+        entry_field = tk.Spinbox(entry_frame, from_=1, to=100, textvariable=self.entry_var, width=10,
+                                 font=self.regular_font)
         entry_field.grid(row=0, column=1)
 
+        # Добавляем горизонтальную линию после поля ввода
+        separator1 = tk.Frame(self.root, height=2, bd=1, relief="groove", bg="#ADD8E6")
+        separator1.pack(fill="x", pady=10)
+
         # Frame to contain the checkboxes
-        checkbox_frame = tk.Frame(self.root)
+        checkbox_frame = tk.Frame(self.root, bg="#ADD8E6")
         checkbox_frame.pack(pady=20)
 
         # Create 17 checkboxes with sequential tasks
@@ -47,11 +53,19 @@ class MyApp:
                 text=f"Задание {i + 1}",
                 font=self.regular_font,
                 variable=val,
+                bg="#ADD8E6",
+                activebackground="#87CEFA",  # Изменяем цвет фона при наведении
+                highlightthickness=0,  # Убираем рамку при наведении
+                selectcolor="#87CEFA"  # Изменяем цвет фона при выборе
             )
 
             checkbox.select()
             checkbox.grid(row=i // 5, column=i % 5, sticky="w")
             self.check_vals.append(val)
+
+        # Добавляем горизонтальную линию после чекбоксов
+        separator2 = tk.Frame(self.root, height=2, bd=1, relief="groove", bg="#ADD8E6")
+        separator2.pack(fill="x", pady=10)
 
         # Checkbox to check/uncheck all checkboxes
         check_var = tk.BooleanVar()
@@ -60,13 +74,22 @@ class MyApp:
             text="Выделить все",
             font=self.regular_font,
             variable=check_var,
-            command=lambda: self.check_uncheck_all(check_var)
+            command=lambda: self.check_uncheck_all(check_var),
+            bg="#ADD8E6",
+            activebackground="#87CEFA",  # Изменяем цвет фона при наведении
+            highlightthickness=0,  # Убираем рамку при наведении
+            selectcolor="#87CEFA"  # Изменяем цвет фона при выборе
         )
         check_all_checkbox.select()
         check_all_checkbox.pack()
 
+        # Добавляем горизонтальную линию после кнопки "Выделить все"
+        separator3 = tk.Frame(self.root, height=2, bd=1, relief="groove", bg="#ADD8E6")
+        separator3.pack(fill="x", pady=10)
+
         # Button to save file
-        save_button = tk.Button(self.root, text="Сгенерировать", font=self.regular_font, command=self.save_file)
+        save_button = tk.Button(self.root, text="Сгенерировать", font=self.regular_font, command=self.save_file,
+                                bg="#ADD8E6", activebackground="#87CEFA", )
         save_button.pack(pady=20)
 
     def save_file(self):
