4476509ca4c774ef15178d09a4e346235d357cef
ChristinaBel <kristina04bel@gmail.com>
2024-05-19 17:15:14 +0300

    fix main

--- main.py
+++ main.py
@@ -120,8 +120,10 @@ class MyApp:
         try:
             os.remove("Варианты.docx")
             os.remove("Ответы.docx")
-        except OSError:
-            pass
+        except OSError as e:
+            messagebox.showerror("Ошибка", f"Невозможно сгенерировать новые файлы. \n"
+                                           "Пожалуйста, закройте открытые файлы и попробуйте снова.")
+            return
 
         # Create files for vars and answers
         path_var = "Варианты.docx"
@@ -135,13 +137,24 @@ class MyApp:
 
         i = 0
         while i < var_count:
-            # Create base files
-            # Every function edits and adds its task to the output file
-            # If corresponding check is checked
             odd_gen.generate_odd(self.check_vals, os.path.abspath(path_var), os.path.abspath(path_ans), i + 1)
+            if i < var_count - 1:  # Добавляем разрыв страницы, если это не последний вариант
+                doc1 = docx.Document(os.path.abspath(path_var))
+                doc1.add_page_break()
+                doc1.save(path_var)
+                doc2 = docx.Document(os.path.abspath(path_ans))
+                doc2.add_page_break()
+                doc2.save(path_ans)
             i += 1
             if i < var_count:
                 even_gen.generate_even(self.check_vals, os.path.abspath(path_var), os.path.abspath(path_ans), i + 1)
+                if i < var_count - 1:  # Добавляем разрыв страницы, если это не последний вариант
+                    doc1 = docx.Document(os.path.abspath(path_var))
+                    doc1.add_page_break()
+                    doc1.save(path_var)
+                    doc2 = docx.Document(os.path.abspath(path_ans))
+                    doc2.add_page_break()
+                    doc2.save(path_ans)
                 i += 1
 
         messagebox.showinfo("Файл сохранен", "Файл был успешно сохранен.")
