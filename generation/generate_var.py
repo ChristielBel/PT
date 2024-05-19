ff97def74ea8714056c37cbddc8563df3fc5253c
Maksim Shiryaev <shimady563@gmail.com>
2024-05-19 18:26:58 +0300

    feat(generation): add brakes to docs

--- generation/generate_var.py
+++ generation/generate_var.py
@@ -1,9 +1,20 @@
+import docx
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT as align
 
 from generation import writer
 
 
 def generate_var(task_functions, ans_functions, check_values, doc_path, ans_path, num):
+
+    # Add brakes
+    if num > 1:
+        doc1 = docx.Document(doc_path)
+        doc1.add_page_break()
+        doc1.save(doc_path)
+        doc1 = docx.Document(ans_path)
+        doc1.add_page_break()
+        doc1.save(ans_path)
+
     # Writing header
     header = "Вариант " + str(num) + '\n'
 
